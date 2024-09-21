6feed1b2e8f631d00a3a7929c0cea10caca5e6d2
Damian Bergemann <me@headlessdev.de>
2024-09-21 20:48:20 +0200

    API fix

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend-URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
