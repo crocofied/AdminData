20aef9e37ef33b4a4f0e87cae8d2b72e44d5cc55
Damian Bergemann <me@headlessdev.de>
2024-09-21 20:43:56 +0200

    CORS Fix

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],  # Frontend-URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
