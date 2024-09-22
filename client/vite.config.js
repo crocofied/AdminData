4e1e41d340564b974965c4c815e5aa7fc5f858b9
Damian Bergemann <me@headlessdev.de>
2024-09-22 15:03:15 +0200

    ViteConifg

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 80,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
 })
