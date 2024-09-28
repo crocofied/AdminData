2b05eb968bac820817cdb4b66680cc446d57a3bf
Damian Bergemann <me@headlessdev.de>
2024-09-28 12:43:16 +0200

    Tablew View

--- client/src/pages/TableView.jsx
+++ client/src/pages/TableView.jsx
@@ -6,11 +6,8 @@ import SessionChecker from '../components/SessionChecker';
 import Navbar from '../components/Navbar';
 
 const TableView = () => {
-    // Navigation and location details
     const navigate = useNavigate();
     const location = useLocation();
-
-    // Connection details
     const [connectionID, setConnectionID] = useState();
     const [connectionName, setConnectionName] = useState('');
     const [databaseName, setDatabaseName] = useState('');
@@ -20,6 +17,9 @@ const TableView = () => {
     const [errorVisible, setErrorVisible] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const [maxPage, setMaxPage] = useState(1);
+    const [selectedRow, setSelectedRow] = useState(null);
+    const [selectedOldRow, setSelectedOldRow] = useState(null);
+    const [selectedNewRow, setSelectedNewRow] = useState({ values: {} });
 
     useEffect(() => {
         if (!SessionChecker()) {
@@ -28,36 +28,102 @@ const TableView = () => {
     }, [navigate]);
 
     useEffect(() => {
-        setConnectionID(location.state.connection_id);
-        setConnectionName(location.state.connection_name);
-        setDatabaseName(location.state.database_name);
-        setTableName(location.state.table_name);
+        if (location.state) {
+            setConnectionID(location.state.connection_id);
+            setConnectionName(location.state.connection_name);
+            setDatabaseName(location.state.database_name);
+            setTableName(location.state.table_name);
+        }
     }, [location.state]);
 
-    useEffect(() => {
+    const retrieveTableData = () => {
         if (connectionID && databaseName && tableName) {
-            axios
-                .post(
-                    `${import.meta.env.VITE_API_URL}/get_table_data_values?page=${currentPage}&size=7`,
-                    {
-                        session_id: Cookies.get('session_id'),
-                        connection_id: connectionID,
-                        database_name: databaseName,
-                        table_name: tableName,
-                    }
-                )
+            axios.post(`${import.meta.env.VITE_API_URL}/get_table_data_values?page=${currentPage}&size=7`, {
+                    session_id: Cookies.get('session_id'),
+                    connection_id: connectionID,
+                    database_name: databaseName,
+                    table_name: tableName,
+                })
                 .then((response) => {
-                    setData(response.data.items);
-                    setMaxPage(response.data.pages);
-                    console.log(response.data);
+                    setData(response.data.items || []); // Set empty array if items is undefined
+                    setMaxPage(response.data.pages || 1); // Set to 1 if pages is undefined
+                    console.log(response.data)
                 })
                 .catch((error) => {
                     setError(error.message);
                     setErrorVisible(true);
-                    console.log(error);
                 });
         }
+    };
+
+    useEffect(() => {
+        retrieveTableData();
     }, [connectionID, databaseName, tableName, currentPage]);
+
+    const handleEdit = () => {
+        axios.post(`${import.meta.env.VITE_API_URL}/edit_table_data`, {
+            session_id: Cookies.get('session_id'),
+            connection_id: connectionID,
+            database_name: databaseName,
+            table_name: tableName,
+            row: selectedOldRow,  // use old row data
+            new_row: selectedRow,  // use updated row data
+        })
+        .then((response) => {
+            retrieveTableData();
+            document.getElementById('my_modal_3').close();
+        })
+        .catch((error) => {
+            setError(error.message);
+            setErrorVisible(true);
+        });
+    };
+
+    const handleDelete = (row) => {
+        axios.post(`${import.meta.env.VITE_API_URL}/delete_table_data`, {
+            session_id: Cookies.get('session_id'),
+            connection_id: connectionID,
+            database_name: databaseName,
+            table_name: tableName,
+            row: row,
+        })
+        .then((response) => {
+            retrieveTableData();
+        })
+        .catch((error) => {
+            setError(error.message);
+            setErrorVisible(true);
+        });
+    };
+
+    const handleAdd = () => {
+        axios.post(`${import.meta.env.VITE_API_URL}/add_table_data`, {
+            session_id: Cookies.get('session_id'),
+            connection_id: connectionID,
+            database_name: databaseName,
+            table_name: tableName,
+            row: selectedNewRow,
+        })
+        .then((response) => {
+            retrieveTableData();
+            document.getElementById('my_modal_4').close();
+        })
+        .catch((error) => {
+            setError(error.message);
+            setErrorVisible(true);
+        });
+    };
+
+    const openEditModal = (row) => {
+        setSelectedOldRow(row);  // save old row data
+        setSelectedRow({ ...row });  // create a copy of the row for editing
+        document.getElementById('my_modal_3').showModal();
+    };
+
+    useEffect(() => {
+        console.log(selectedOldRow);
+        console.log(selectedRow);
+    }, [selectedOldRow, selectedRow]);
 
     return (
         <>
@@ -108,23 +174,29 @@ const TableView = () => {
                                 <>
                                     <thead>
                                         <tr>
-                                            {Object.keys(data[0].values).map((key) => (
-                                                <th key={key}>{key}</th>
+                                            {Object.keys(data[0].values || {}).map((key) => (
+                                                <th className='text-xl' key={key}>{key}</th>
                                             ))}
+                                            <th className='text-xl'>Actions</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {data.map((row) => (
                                             <tr key={row.id}>
-                                                {Object.values(row.values).map((value, index) => (
+                                                {Object.values(row.values || {}).map((value, index) => (
                                                     <td key={index}>{value !== null ? value : ''}</td>
                                                 ))}
+                                                <td>
+                                                    <button onClick={() => openEditModal(row)} className="btn btn-neutral mr-2">Edit</button>
+                                                    <button onClick={() => handleDelete(row)} className="btn btn-neutral">Delete</button>
+                                                </td>
                                             </tr>
                                         ))}
                                     </tbody>
                                 </>
                             )}
                         </table>
+                        <button className="btn btn-neutral mt-5 w-full" onClick={() => document.getElementById('my_modal_4').showModal()}>Add Row</button>
                         <div className="flex justify-center items-center join pt-5">
                             {currentPage === 1 ? (
                                 <button className="join-item btn" disabled>
@@ -147,6 +219,74 @@ const TableView = () => {
                             )}
                         </div>
                     </div>
+
+                    <dialog id="my_modal_3" className="modal">
+                        <div className="modal-box">
+                            <form method="dialog">
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                            </form>
+                            <h3 className="font-bold text-lg">Edit Table Data</h3>
+                            <div className="divider"></div>
+                            {selectedRow && (
+                                <form method="dialog">
+                                    <div className='space-y-2'>
+                                        {Object.keys(selectedRow.values || {}).map((key) => (
+                                            <div className="form-control" key={key}>
+                                                <label className="input input-bordered flex items-center gap-2">
+                                                    {key}
+                                                    <input type="text" defaultValue={selectedRow.values[key]} className="grow" onChange={(e) => {
+                                                        setSelectedRow(prev => ({
+                                                            ...prev,
+                                                            values: {
+                                                                ...prev.values,
+                                                                [key]: e.target.value // Update the selectedRow values
+                                                            }
+                                                        }));
+                                                    }} />
+                                                </label>
+                                            </div>
+                                        ))}
+                                        <div className="form-actions">
+                                            <button className="btn btn-primary w-full" type="button" onClick={handleEdit}>Save</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            )}
+                        </div>
+                    </dialog>
+
+                    <dialog id="my_modal_4" className="modal">
+                        <div className="modal-box">
+                            <form method="dialog">
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                            </form>
+                            <h3 className="font-bold text-lg">Add Table Data</h3>
+                            <div className="divider"></div>
+                            <form method="dialog">
+                                <div className='space-y-2'>
+                                    {data.length > 0 && Object.keys(data[0].values || {}).map((key) => (
+                                        <div className="form-control" key={key}>
+                                            <label className="input input-bordered flex items-center gap-2">
+                                                {key}
+                                                <input type="text" className="grow" onChange={(e) => {
+                                                    setSelectedNewRow(prev => ({
+                                                        ...prev,
+                                                        values: {
+                                                            ...prev.values,
+                                                            [key]: e.target.value // Update the selectedNewRow values
+                                                        }
+                                                    }));
+                                                }} />
+                                            </label>
+                                        </div>
+                                    ))}
+                                    <div className="form-actions">
+                                        <button className="btn btn-primary w-full" type="button" onClick={handleAdd}>Add Row</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </dialog>
                 </div>
             </div>
         </>
