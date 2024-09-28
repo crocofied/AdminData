a37129d3e014bc33d9745814b9a3d5addc616dea
Damian Bergemann <me@headlessdev.de>
2024-09-28 16:31:21 +0200

    Docs Meta

--- documentation/docs/.vitepress/config.js
+++ documentation/docs/.vitepress/config.js
@@ -2,6 +2,23 @@
 export default {
     title: "AdminData",
     description: "A secure and modern self hosted Database Client",
+    lastUpdated: true,
+    head: [
+        ["link", { rel: "icon",  type: "image/png", href: "/logo.png" }],
+        ["meta", { name: "theme-color", content: "#7482f7" }],
+        ["meta", {property: "og:type", content: "website"}],
+        ["meta", {property: "og:locale", content: "en"}],
+        ["meta", {
+            property: "og:title",
+            content: "AdminData | A secure and modern self hosted Database Client",
+        }],
+        ["meta", { property: "og:site_name", content: "AdminData" }],
+        ["meta", { property: "og:image", content: "/logo.png" }],
+        ["meta", { property: "og:image:type", content: "image/png" }],
+        ["meta", { property: "twitter:card", content: "summary_large_image" }],
+        ["meta", { property: "twitter:image:src", content: "/thumbnail.png" }],
+        ["meta", { property: "og:url", content: "https://admindata.xyz" }],
+    ],
     themeConfig: {
         logo: "/logo.png",
         siteTitle: "AdminData",
