a29ab60b45cadcc95e2e22aab9fb13649ab89bcf
Damian Bergemann <damian@headlessdev.de>
2024-10-05 11:25:35 +0200

    API Handler Fix

--- client/src/common/utils/api.js
+++ client/src/common/utils/api.js
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const makePostRequest = async (endpoint, data = {}) => {
   try {
     const response = await axios.post(`${API_BASE_URL}${endpoint}`, data);
-    return response.data;
+    return response;
   } catch (error) {
     console.error('Error making POST request:', error);
     throw error;
