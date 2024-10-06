b04c077981c08f1cd6f56f42d0fa20a16fb7c22b
Damian Bergemann <damian@headlessdev.de>
2024-10-06 15:30:57 +0200

    Home Localization

---
+++ client/src/i18n.js
@@ -0,0 +1,11 @@
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import enJSON from './locale/en.json'
+import deJSON from './locale/de.json'
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { ...enJSON },
+    de: { ...deJSON },
+  },
+  lng: "en",
+});
